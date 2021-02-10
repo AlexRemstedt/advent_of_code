4da7b4745dc37ecd221f3a085e3ff82e4190b11d
AlexRemstedt <49374085+AlexRemstedt@users.noreply.github.com>
2021-02-10 22:37:49 +0100

    this should work

---
+++ day_4_alt.py
@@ -0,0 +1,30 @@
+batch_file = [
+	'ecl:gry pid:860033327 eyr:2020 hcl:#fffffd',
+	'byr:1937 iyr:2017 cid:147 hgt:183cm',
+	'',
+	'iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884',
+	'hcl:#cfa07d byr:1929',
+	'',
+	'hcl:#ae17e1 iyr:2013',
+	'eyr:2024',
+	'ecl:brn pid:760753108 byr:1931',
+	'hgt:179cm',
+	'',
+	'hcl:#cfa07d eyr:2025 pid:166559648',
+	'iyr:2011 ecl:brn hgt:59in'
+]
+
+cool_store = []
+
+for n in batch_file:
+	x = n.split()
+	if x:
+		cool_store += x
+
+fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+
+cool_store.sort()
+m = []
+for var in fields:
+	m.append(sum(var in s for s in cool_store))
+print(min(m))
