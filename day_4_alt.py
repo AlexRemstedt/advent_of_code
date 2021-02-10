f1ad02f502cb7dff7614489ad736a60a238a00eb
AlexRemstedt <49374085+AlexRemstedt@users.noreply.github.com>
2021-02-10 22:43:13 +0100

    it doesnt work :O

--- day_4_alt.py
+++ day_4_alt.py
@@ -1,18 +1,6 @@
-batch_file = [
-	'ecl:gry pid:860033327 eyr:2020 hcl:#fffffd',
-	'byr:1937 iyr:2017 cid:147 hgt:183cm',
-	'',
-	'iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884',
-	'hcl:#cfa07d byr:1929',
-	'',
-	'hcl:#ae17e1 iyr:2013',
-	'eyr:2024',
-	'ecl:brn pid:760753108 byr:1931',
-	'hgt:179cm',
-	'',
-	'hcl:#cfa07d eyr:2025 pid:166559648',
-	'iyr:2011 ecl:brn hgt:59in'
-]
+file4 = open('input_files/day_4_input', 'r')
+batch_file = list(map(str, file4.readlines()))
+file4.close()
 
 cool_store = []
 
