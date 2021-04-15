b83fce183c8f3aa5253a33fe8ba426ce6854c496
AlexRemstedt <49374085+AlexRemstedt@users.noreply.github.com>
2021-04-15 17:06:18 +0200

    update(input): remove file..close()

--- day_4.py
+++ day_4.py
@@ -4,29 +4,13 @@
 		Github repo: (https://github.com/AlexRemstedt/advent_of_code)
 """
 
-# === Input ===
-file4 = open('input_files/day_4_input', 'r')
-batch_file = list(map(str, file4.read().splitlines()))
-file4.close()
-
 # === Variables ===
 valid_passports = 0
 
-# batch_file = [
-# 	'ecl:gry pid:860033327 eyr:2020 hcl:#fffffd',
-# 	'byr:1937 iyr:2017 cid:147 hgt:183cm',
-# 	'',
-# 	'iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884',
-# 	'hcl:#cfa07d byr:1929',
-# 	'',
-# 	'hcl:#ae17e1 iyr:2013',
-# 	'eyr:2024',
-# 	'ecl:brn pid:760753108 byr:1931',
-# 	'hgt:179cm',
-# 	'',
-# 	'hcl:#cfa07d eyr:2025 pid:166559648',
-# 	'iyr:2011 ecl:brn hgt:59in'
-# ]
+# === Input ===
+with open('input_files/day_4_input', 'r') as batch:
+	batch_file = list(map(str, batch.read().splitlines()))
+	pass
 
 
 # === Passport Class ===
