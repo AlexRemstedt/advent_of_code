3fbce53e184f7cb9a59de2675648efb9004ad2f6
AlexRemstedt <alex@remstedt.com>
2021-12-11 22:43:27 +0100

    Day 1 finished

---
+++ 2021/1_SonarSweep.py
@@ -0,0 +1,42 @@
+"""Day 1.
+
+How many measurements are larger than the previous measurment.
+
+Notes: origineel zelf gemaakt; 
+verbeterd met https://mathspp.com/blog/advent-of-code-sonar-sweep-analysis
+"""
+
+
+from itertools import tee
+
+
+def pairwise(it):
+    """Mock `itertools.pairwise` for python version below 3.10."""
+    prev_, next_ = tee(it, 2)
+    next(next_)
+    yield from zip(prev_, next_)
+
+
+# Task 1
+with open("data/1_SonarSweep", 'r') as raw_inputs:
+    # integer-ify 
+    # use brackets for generator-expression
+    depths = (int(line) for line in raw_inputs)
+
+    # voor zip() neemt hij altijd de grootte van de kleinste
+    count1 = sum(prev_ < next_ for prev_, next_ in pairwise(depths))
+
+
+# Task 2 (Also generalised.)
+window_size = 3
+with open("data/1_SonarSweep", 'r') as raw_inputs:
+    depths = (int(line) for line in raw_inputs)
+    window = [next(depths) for _ in range(window_size)]
+    count = 0
+    for elem in depths:
+        window.append(elem)
+        if window[0] < window[-1]:
+            count += 1
+        window.pop(0)
+
+    print(count)
