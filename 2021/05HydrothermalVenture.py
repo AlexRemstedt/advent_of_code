589c75ca63e2f90e5fb1d50a202d7d78b45e0aa3
AlexRemstedt <alex@remstedt.com>
2021-12-19 23:58:29 +0100

    feat(draw_lines): vertical and horizontal lines can be drawn.

--- 2021/05HydrothermalVenture.py
+++ 2021/05HydrothermalVenture.py
@@ -18,13 +18,15 @@ def data_man():
             coord_pair[n] = coord.split(',')
             for m, val in enumerate(coord_pair[n]):
                 coord_pair[n][m] = int(val)
+    return lines
 
-# make grid_map
+    
+
 class GridMap:
     """Contain GridMap for lines."""
 
     def __init__(self, size):
-        self.grid = np.array(shape=size)
+        self.grid = np.zeros(shape=size)
 
     @classmethod
     def from_lines(cls, lines):
@@ -40,9 +42,26 @@ class GridMap:
                         max_ = value
         return cls(size=(max_, max_))
 
+    def draw_vertical(self, a, b):
+        """Draw vertical lines in the grid."""
+        if a[0] != b[0]:  # checks whether y is equal
+            pass
+        for x_coord in range(a[1], b[1])
+            self.grid[a[0]][x] += 1
+        return "Line drawn"
+
+    def draw_horizontal(self, a, b):
+        """Draw horizontal lines in the grid."""
+        if a[1] != b[1]:  # checks whether y is equal
+            pass
+        for x_coord in range(a[0], b[0])
+            self.grid[x][a[1]] += 1
+        return "Line drawn"
+
+
 
 def main():
-    grid_map = np.zeros(shape=(max_,max_))
+    grid_map = GridMap.from_lines(lines)
 
 if __name__ == "__main__":
     main()
