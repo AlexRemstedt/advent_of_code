c00a44865e2ed01f5570244ecb471c6e6bbe3f4e
AlexRemstedt <alex@remstedt.com>
2021-12-25 22:47:54 +0100

    feat(coordinate): create coordinate class. refactor(data_man): data_man -> data_manipulation

--- 2021/05HydrothermalVenture.py
+++ 2021/05HydrothermalVenture.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-def data_man():
+def data_manipulation():
     """Get data from inputfile."""
     # Main script
     with open('./inputs/5_HydrothermalVenture', 'r') as f:
@@ -42,26 +42,35 @@ class GridMap:
                         max_ = value
         return cls(size=(max_, max_))
 
-    def draw_vertical(self, a, b):
+    def draw_vertical(self, a: Coordinate, b: Coordinate):
         """Draw vertical lines in the grid."""
-        if a[0] != b[0]:  # checks whether y is equal
+        if a[0] != b[0]:  # checks whether x is equal
             pass
-        for x_coord in range(a[1], b[1])
+        for x_coord in range(a[1], b[1]):
             self.grid[a[0]][x] += 1
         return "Line drawn"
 
-    def draw_horizontal(self, a, b):
+    def draw_horizontal(self, a: Coordinate, b: Coordinate):
         """Draw horizontal lines in the grid."""
         if a[1] != b[1]:  # checks whether y is equal
             pass
-        for x_coord in range(a[0], b[0])
+        for x_coord in range(a[0], b[0]):
             self.grid[x][a[1]] += 1
         return "Line drawn"
 
 
+class Coordinate:
+    """Coordinate object."""
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 
 def main():
+    lines = data_manipulation()
     grid_map = GridMap.from_lines(lines)
+    print(grid_map.grid)
 
 if __name__ == "__main__":
     main()
