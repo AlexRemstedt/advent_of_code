b896e63c2627d75e5989addd54297a2ca912b943
AlexRemstedt <alex@remstedt.com>
2021-12-28 23:28:41 +0100

    feat(GridMap): add line drawer. feat(Line): create line object.

--- 2021/05HydrothermalVenture.py
+++ 2021/05HydrothermalVenture.py
@@ -1,6 +1,8 @@
 """Day 5: Hydrothermal Venture."""
 
+from typing import List
 import numpy as np
+
 
 def data_manipulation():
     """Get data from inputfile."""
@@ -11,7 +13,7 @@ def data_manipulation():
     for coord_pair in raw:
         lines.append(coord_pair.split())
 
-    # make data nice to work with
+    # Make data nice to work with
     for coord_pair in lines:
         del coord_pair[1]
         for n, coord in enumerate(coord_pair):
@@ -19,10 +21,9 @@ def data_manipulation():
             for m, val in enumerate(coord_pair[n]):
                 coord_pair[n][m] = int(val)
     return lines
-
     
 
-# make grid_map
+# Make grid_map
 class GridMap:
     """Contain GridMap for lines."""
 
@@ -43,28 +44,22 @@ class GridMap:
                         max_ = value
         return cls(size=(max_, max_))
 
-    def draw_vertical(self, a: Coordinate, b: Coordinate):
-        """Draw vertical lines in the grid."""
-        if a[0] != b[0]:  # checks whether x is equal
-            pass
-        for x_coord in range(a[1], b[1]):
-            self.grid[a[0]][x] += 1
-        return "Line drawn"
 
-    def draw_horizontal(self, a: Coordinate, b: Coordinate):
-        """Draw horizontal lines in the grid."""
-        if a[1] != b[1]:  # checks whether y is equal
-            pass
-        for x_coord in range(a[0], b[0]):
-            self.grid[x][a[1]] += 1
-        return "Line drawn"
+class Line:
+    """Line object."""
 
+    def __init__(self, point_1: List[float], point_2: List[float]):
+        self.a = tuple(point_1)
+        self.b = tuple(point_2)
 
 
 def main():
-    lines = data_manipulation()
-    grid_map = GridMap.from_lines(lines)
-    print(grid_map.grid)
+    """Main function."""
+    basic_lines = data_manipulation()
+    lines = []
+    for line in basic_lines:
+        lines.append(Line(line[0], line[1]))
+    print(lines)
 
 if __name__ == "__main__":
     main()
