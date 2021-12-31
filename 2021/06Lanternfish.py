e66c5c3f106c4ecca9fd175b1ef5e9f2d0c6ed15
AlexRemstedt <alex@remstedt.com>
2022-01-01 00:31:43 +0100

    Data of day 6 added

--- 2021/06Lanternfish.py
+++ 2021/06Lanternfish.py
@@ -1,27 +1,56 @@
 """Day 6: Lanternfish."""
 
+from collections import defaultdict
+
 def from_input(path_to_file='./inputs/6_Lanternfish'):
     """Get data from input."""
     with open(path_to_file, 'r') as f:
-        [raw] = [l.split() for l in f]
-    [raw] = [string.split(',') for string in raw]
-    raw = [int(num) for num in raw]
+        raw = [int(num) for num in f.read().strip().split(',')]
     return raw
 
-def main():
+def part1():
     initial_state = from_input()
     
+    DAYS = 80
     lantern_fish = initial_state.copy()
-    days = [*range(1, 81)]
-    for day in days:
+    for day in range(DAYS):
         for n, fish in enumerate(lantern_fish):
             lantern_fish[n] -= 1
             if fish == 0:
                 lantern_fish.append(9)
                 lantern_fish[n] = 6
-    print(len(lantern_fish))
+    return len(lantern_fish)
+
+
+def part2():
+    initial_state = from_input()
+
+    lantern_fish = initial_state.copy()
+
+    fish_map = {}
+    for fish in lantern_fish:
+        if fish not in fish_map:
+            fish_map[fish] = 0
+        fish_map[fish] += 1
+
+    DAYS = 256
+
+    for day in range(DAYS):
+        up_fish_map = defaultdict(int)  # Avoids KeyError
+
+        # Go to each fish to change its state.
+        for fish, count in fish_map.items():
+            if fish == 0:
+                up_fish_map[6] += count
+                up_fish_map[8] += count
+            else:
+                up_fish_map[fish - 1] += count
+        
+        fish_map = up_fish_map
+    return(sum(fish_map.values()))
 
 if __name__ == "__main__":
-    main()
+    print(part1())
+    print(part2())
 
 
