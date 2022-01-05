261b96e268add95ad4386c445d152242172a627a
AlexRemstedt <alex@remstedt.com>
2022-01-05 19:05:31 +0100

    cleanup(day8): long documentation added to README.md.

--- 2021/08SevenSegmentSearch.py
+++ 2021/08SevenSegmentSearch.py
@@ -1,19 +1,14 @@
-#!/venv/bin/python
 """Day 8: Seven Segment Search."""
 
 import itertools
 from collections import defaultdict 
 
-def get_input(path_to_file='./inputs/8_SevenSegmentSearch'):
-    with open(path_to_file, 'r') as f:
-        raw = f.readlines()
-    raw = [line.split('|')[1].strip() for line in raw]
-    raw = [words.split() for words in raw]
-    return raw
-
-
-def part1():
-    lines = get_input()
+def part1(path_to_file='./inputs/8_SevenSegmentSearch'):
+    lines = []
+    for line in open(path_to_file, 'r'):
+        lines.append(line)
+    lines = [line.split('|')[1].strip() for line in lines]
+    lines = [words.split() for words in lines]
 
     ez_digits = [2, 4, 3, 7]
     count = 0
@@ -22,24 +17,6 @@ def part1():
             if len(digit) in ez_digits:
                 count += 1
     return count
-
-
-
-# def part1(path_to_file='./inputs/8_SevenSegmentSearch'):
-    # ans = 0
-    # for line in open(path_to_file, 'r'):
-        # before, after = line.split('|')
-        # before = before.split()
-        # after = after.split()
-        # # Store lengths and words.
-        # by_len = defaultdict(list)
-        # for word in before:
-            # by_len[len(word)].append(word)
-        # for word in after:
-            # # If word is unique, then there is only 1 of them.
-            # if len(by_len[len(word)]) == 1: 
-                # ans += 1
-    # print(ans)
 
 
 def part2(path_to_file='./inputs/8_SevenSegmentSearch'):
@@ -169,10 +146,10 @@ def part2(path_to_file='./inputs/8_SevenSegmentSearch'):
             assert len(digit) == 1
             after_digit += str(digit[0])
         ans += int(after_digit)
-    print(ans)
+    return ans
 
 
 if __name__ == "__main__":
-    part1()
-    part2()
+    print(part1())
+    print(part2())
 
