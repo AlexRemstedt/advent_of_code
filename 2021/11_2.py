7dd5481b3d17c96506d9c1db2ce40daabfa9d565
AlexRemstedt <alex@remstedt.com>
2022-04-03 23:21:50 +0200

    feat(from_input): create grid from input_file.

--- 2021/11_2.py
+++ 2021/11_2.py
@@ -1,5 +1,18 @@
 """Day 11: Dumbo Octopus."""
 
+"""
+5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526
+"""
+
 import sys
 import numpy as np
 
@@ -20,7 +33,15 @@ class OctopusGrid:
         self.matrix = octo_matrix
 
     @classmethod
-    def from_input(cls, input_):
-        grid = _
+    def from_input(cls, path):
+        grid = [[Octopus(int(n)) for n in line.strip()] for line in open(path, 'r')]
+        print(f"{grid=}")
         return cls(grid)
+
+def main():
+    input_file = sys.argv[1] if len(sys.argv) > 1 else '9.in'
+    grid = OctopusGrid.from_input(input_file)
+
+if __name__ == "__main__":
+    main()
 
