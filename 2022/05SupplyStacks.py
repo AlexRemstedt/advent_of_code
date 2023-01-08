da7d19ae280b4dc8963b66074bd752d5512ffdf6
Alex Remstedt <alexremstedt@AlexRemstedt>
2023-01-08 22:11:17 +0100

    start day 5

---
+++ 2022/05SupplyStacks.py
@@ -0,0 +1,16 @@
+"""Day 5: Supply Stacks."""
+import sys
+
+
+def part_1(path):
+    pass
+
+
+def part_2(path):
+    pass
+
+
+if __name__ == "__main__":
+    input_file = sys.argv[1] if len(sys.argv) > 1 else '03.x'
+    part_1(input_file)
+    part_2(input_file)
